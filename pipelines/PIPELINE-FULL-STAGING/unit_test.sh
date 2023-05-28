c87b11c453cf2cb1b18b3a370defcbfcb8b422f2
dsalvatim <131033763+dsalvatim@users.noreply.github.com>
2023-05-29 01:47:15 +0200

    Update unit_test.sh

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -7,7 +7,7 @@ echo "PYTHONPATH: $PYTHONPATH"
 export DYNAMODB_TABLE=todoUnitTestsTable
 python test/unit/TestToDo.py
 pip show coverage
-coverage run -m pytest --include=src/todoList.py test/unit/TestToDo.py
+coverage run --include=src/todoList.py test/unit/TestToDo.py
 coverage report
 coverage xml
 coverage html
